b5f70accc1967d54fb384bd87fd692aef86e46cc
MandavkarPranjal <pranjalmandavkar10@gmail.com>
2024-05-04 19:14:46 +0530

    Popup Menu

--- app/src/main/java/com/example/assignment/MainActivity.java
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -1,45 +1,52 @@
 package com.example.assignment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editText;
-    private Button addButton;
-    private ListView listView;
-    private ArrayList<String> itemList;
-    private ArrayAdapter<String> adapter;
+    private Button buttonShowPopup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = findViewById(R.id.editText);
-        addButton = findViewById(R.id.addButton);
-        listView = findViewById(R.id.listView);
+        buttonShowPopup = findViewById(R.id.buttonShowPopup);
 
-        itemList = new ArrayList<>();
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemList);
-        listView.setAdapter(adapter);
-
-        addButton.setOnClickListener(new View.OnClickListener() {
+        buttonShowPopup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String item = editText.getText().toString();
-                if (!item.isEmpty()) {
-                    itemList.add(item);
-                    adapter.notifyDataSetChanged();
-                    editText.setText("");
+                showPopupMenu(v);
+            }
+        });
+    }
+
+    private void showPopupMenu(View v) {
+        PopupMenu popupMenu = new PopupMenu(this, v);
+        popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_edit:
+                        Toast.makeText(MainActivity.this, "Edit clicked", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.action_delete:
+                        Toast.makeText(MainActivity.this, "Delete clicked", Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:
+                        return false;
                 }
             }
         });
+
+        popupMenu.show();
     }
 }
