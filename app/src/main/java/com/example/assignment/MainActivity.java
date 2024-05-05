ebf0cf70e5a81a8d095c8be9ae930432ca108cdc
MandavkarPranjal <pranjalmandavkar10@gmail.com>
2024-05-05 18:06:35 +0530

    unsuccessfull trails

--- app/src/main/java/com/example/assignment/MainActivity.java
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -1,71 +1,58 @@
 package com.example.assignment;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText usernameEdit, passwordEdit;
-    CheckBox checkBox1, checkBox2, checkBox3;
-    RadioGroup genderRadioGroup;
-    Button registerButton;
+    private EditText editTextDish, editTextQuantity;
+    private Button buttonAdd;
+    private TextView textViewBill;
+    private Map<String, Double> menu;
+    private double totalBill;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        usernameEdit = findViewById(R.id.username);
-        passwordEdit = findViewById(R.id.password);
-        checkBox1 = findViewById(R.id.checkBox1);
-        checkBox2 = findViewById(R.id.checkBox2);
-        checkBox3 = findViewById(R.id.checkBox3);
-        genderRadioGroup = findViewById(R.id.genderRadioGroup);
-        registerButton = findViewById(R.id.button);
+        editTextDish = findViewById(R.id.editTextDish);
+        editTextQuantity = findViewById(R.id.editTextQuantity);
+        buttonAdd = findViewById(R.id.buttonAdd);
+        textViewBill = findViewById(R.id.textViewBill);
 
-        registerButton.setOnClickListener(new View.OnClickListener() {
+        menu = new HashMap<>();
+        menu.put("Coffee", 2.50);
+        menu.put("Tea", 1.50);
+        menu.put("Sandwich", 5.00);
+        menu.put("Cake", 3.00);
+
+        totalBill = 0.0;
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String username = usernameEdit.getText().toString();
-                String password = passwordEdit.getText().toString();
+            public void onClick(View v) {
+                String dish = editTextDish.getText().toString();
+                int quantity = Integer.parseInt(editTextQuantity.getText().toString());
 
-                StringBuilder courseSelection = new StringBuilder();
-                if(checkBox1.isChecked()) {
-                    courseSelection.append("BCA");
+                if (menu.containsKey(dish)) {
+                    double price = menu.get(dish);
+                    totalBill += price * quantity;
+                    updateBill();
                 }
-                if (checkBox2.isChecked()) {
-                    courseSelection.append("MCA");
-                }
-                if (checkBox3.isChecked()) {
-                    courseSelection.append("MBA");
-                }
-
-                int genderId = genderRadioGroup.getCheckedRadioButtonId();
-                RadioButton selectedGender = findViewById(genderId);
-                String gender = selectedGender != null ? selectedGender.getText().toString() : "Gender not selected";
-
-                String message = "Username: " + username + "\nPassword: " + password + "\nCourse Selection: " + (courseSelection.length() > 0 ? courseSelection.toString(): "No Course Selectee") + "\nGender: " + gender;
-
-                View layout = getLayoutInflater().inflate(R.layout.content_main, null);
-
-                TextView text = layout.findViewById(R.id.toastText);
-                text.setText(message);
-
-                Toast toast = new Toast(getApplicationContext());
-                toast.setDuration(Toast.LENGTH_LONG);
-                toast.setView(layout);
-                toast.show();
-
             }
         });
+    }
+
+    private void updateBill() {
+        DecimalFormat df = new DecimalFormat("0.00");
+        textViewBill.setText("Total Bill: $" + df.format(totalBill));
     }
 }
